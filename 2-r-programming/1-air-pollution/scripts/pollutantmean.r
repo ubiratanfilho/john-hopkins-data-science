2e5ebd73b680b101af6cae34a4a95e20cdf24c3e
ubiratanfilho <ubfilho16@gmail.com>
2022-03-23 08:23:07 -0300

    Add Data for Air Pollution and add pollutant_mean function

---
+++ 2-r-programming/1-air-pollution/scripts/pollutantmean.r
@@ -0,0 +1,20 @@
+pollutantmean <- function(pollutant, id=1:332, dir=path_data){
+        ### Returns the mean of the specified pollutant in all files within `id`
+        
+        ### Arguments
+        ## dir (character) -> the files directory
+        ## pollutant (character) -> the pollutant we wil return its mean ("sulfate" or "nitrate")
+        ## id (vector of integers) -> the id of the files we will analyze
+        sum_pollutants = 0 # sum of pollutants (excluding NAs)
+        len_pollutants = 0 # number of pollutants (excluding NAs)
+        
+        for (i in id){
+                f = read.csv(path_data[i]) # reading file
+                sum_pollutants <- sum_pollutants + sum(f[pollutant], na.rm = TRUE)
+                len_pollutants <- len_pollutants + nrow(na.omit(f[pollutant]))
+        }
+        
+        mean_pollutants <- sum_pollutants/len_pollutants #  calculating the mean
+        mean_pollutants
+        
+}
---
+++ 2-r-programming/1-air-pollution/scripts/test.r
@@ -0,0 +1,2 @@
+path_data <- list.files("data", full.names = TRUE)
+csv_example <- read.csv("data/003.csv")
